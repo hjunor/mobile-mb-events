84300c595852f3cffc93a51e65b7d8c56530590c
hjunor <hjunor@gmail.com>
2020-07-27 10:52:32 -0300

    add: styles and profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-import { Container } from './styles';
+import { StyleSheet } from 'react-native';
+import {
+  Container,
+  WrapperProfile,
+  WrapperButton,
+  Text,
+  WrapperText,
+  Button,
+  TextButton,
+  WrapperHead,
+  TextHead,
+} from './styles';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <Container>
+      <WrapperHead>
+        <TextHead>Perfil</TextHead>
+      </WrapperHead>
+      <WrapperProfile style={styles.boxShadow}>
+        <WrapperText>
+          <Text>Nome: Heberth Junor</Text>
+        </WrapperText>
+        <WrapperText>
+          <Text>Email: hjunor@gmail.com</Text>
+        </WrapperText>
+        <WrapperButton>
+          <Button>
+            <TextButton>Sair</TextButton>
+          </Button>
+          <Button style={{ backgroundColor: '#d01c2f' }}>
+            <TextButton>Deletar Conta</TextButton>
+          </Button>
+        </WrapperButton>
+      </WrapperProfile>
+    </Container>
   );
 };
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
+    elevation: 5,
+  },
+});
 export default Profile;
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,58 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-items: center;
+  background-color: #fff;
+  padding: 40px;
+  flex: 1;
+`;
+export const WrapperProfile = styled.View`
+  background-color: #fff;
+  width: 300px;
+  height: 300px;
+  border-radius: 8px;
+  margin-top: 80px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Text = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  margin-left: 18px;
+`;
+export const WrapperText = styled.View`
+  background-color: #1a1946;
+  color: #fff;
+
+  justify-content: center;
+  width: 280px;
+  height: 50px;
+  border-radius: 8px;
+  margin: 4px;
+`;
+export const WrapperButton = styled.View`
+  margin-top: 40px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  background-color: #1a1946;
+  margin: 4px;
+  width: 200px;
+  height: 50px;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+`;
+export const TextButton = styled.Text`
+  color: #fff;
+  font-weight: bold;
+`;
+export const WrapperHead = styled.View`
+  margin-top: 20px;
+`;
+export const TextHead = styled.Text`
+  color: #2f55d0;
+  font-weight: bold;
+  font-size: 20px;
+`;
