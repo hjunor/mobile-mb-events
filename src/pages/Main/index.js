0a6032bba8c43851042a5731e9362238f66f590e
hjunor <hjunor@gmail.com>
2020-07-26 23:40:15 -0300

    add: scren main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,75 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Card from '../../components/Card/index';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-// import { Container } from './styles';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import {
+  TextLogo,
+  WrapperTextLogo,
+  WrapperButton,
+  TextButton,
+  Button,
+  Container,
+  WrapperHead,
+  SearchInput,
+  CreateButton,
+} from './styles';
 
-const Main = () => {
+const user = true;
+
+const Main = ({ navigation }) => {
   return (
-    <View>
-      <Text>Main</Text>
-    </View>
+    <Container>
+      <WrapperHead>
+        <SearchInput style={styles.boxShadow}>
+          <AntDesign name="search1" color="#555555" size={19} />
+        </SearchInput>
+        {user === true ? (
+          <CreateButton
+            onPress={() => navigation.navigate('EventRegister')}
+            style={styles.boxShadow}
+          >
+            <AntDesign name="pluscircleo" color="#fff" size={52} />
+          </CreateButton>
+        ) : (
+          <View></View>
+        )}
+      </WrapperHead>
+      <WrapperTextLogo>
+        <TextLogo>MB</TextLogo>
+        <TextLogo>Events</TextLogo>
+      </WrapperTextLogo>
+      <WrapperButton>
+        <Button>
+          <TextButton>Todos Eventos</TextButton>
+        </Button>
+        <Button>
+          <TextButton>Meus Eventos</TextButton>
+        </Button>
+      </WrapperButton>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ScrollView>
+    </Container>
   );
 };
 
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
 export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background-color: #fff;
+  flex: 1;
+`;
+
+export const WrapperHead = styled.View`
+  margin-top: 25px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+`;
+
+export const SearchInput = styled.TextInput`
+  background-color: #fff;
+  width: 80%;
+  height: 40px;
+  margin-right: 10px;
+  border-radius: 16px;
+`;
+
+export const CreateButton = styled.TouchableOpacity`
+  margin-left: 0px;
+  background-color: #2f55d0;
+  color: #fff;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 30px;
+`;
+export const Button = styled.TouchableOpacity`
+  background-color: #1a1946;
+  margin: 20px;
+  height: 63px;
+  width: 166px;
+  padding: 3px;
+  border-radius: 8px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+`;
+
+export const TextButton = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+`;
+export const WrapperButton = styled.View`
+  margin-top: 25px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+`;
+export const TextLogo = styled.Text`
+  color: #2f55d0;
+  font-weight: bold;
+  font-size: 40px;
+  padding: 5px;
+`;
+export const WrapperTextLogo = styled.View`
+  background-color: #fff;
+  flex-direction: row;
+  justify-content: center;
+`;
