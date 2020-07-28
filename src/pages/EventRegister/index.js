78b37e173463e7ac7e6b9ac450fb0ea983e8bf17
hjunor <hjunor@gmail.com>
2020-07-27 21:06:00 -0300

    style: Event Profile

--- src/pages/EventRegister/index.js
+++ src/pages/EventRegister/index.js
@@ -1,14 +1,110 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-// import { Container } from './styles';
+import { StyleSheet, Picker } from 'react-native';
+import {
+  Input,
+  Container,
+  Button,
+  TextButton,
+  WapperInput,
+  InputPrice,
+  InputTickets,
+  InputNumber,
+} from './styles';
 
-const EventRegister = () => {
+const EventRegister = ({ navigation }) => {
   return (
-    <View>
-      <Text>EventRegister</Text>
-    </View>
+    <Container>
+      <Input
+        style={styles.boxShadow}
+        placeholder="Nome"
+        placeholderTextColor="#999CA6"
+      />
+      <WapperInput>
+        <InputTickets
+          style={styles.boxShadow}
+          placeholder="N° Ingressos"
+          placeholderTextColor="#999CA6"
+          keyboardType="email-address"
+        />
+        <InputPrice
+          style={styles.boxShadow}
+          placeholder="Preço"
+          placeholderTextColor="#999CA6"
+        />
+      </WapperInput>
+      <Picker
+        selectedValue={{}}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => {}}
+      >
+        <Picker.Item label="Online" value="Online" />
+        <Picker.Item label="Presencial" value="Presencial" />
+      </Picker>
+      <Input
+        style={styles.boxShadow}
+        placeholder="Cidade"
+        placeholderTextColor="#999CA6"
+      />
+      <Input
+        style={styles.boxShadow}
+        placeholder="Bairro"
+        placeholderTextColor="#999CA6"
+      />
+      <Input
+        style={styles.boxShadow}
+        placeholder="Rua"
+        placeholderTextColor="#999CA6"
+        keyboardType="email-address"
+      />
+      <WapperInput>
+        <InputNumber
+          style={styles.boxShadow}
+          placeholder="Numéro"
+          placeholderTextColor="#999CA6"
+          keyboardType="email-address"
+        />
+        <Picker
+          selectedValue={{}}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) => {}}
+        >
+          <Picker.Item label="Online" value="Online" />
+          <Picker.Item label="Presencial" value="Presencial" />
+        </Picker>
+      </WapperInput>
+      <WapperInput>
+        <Entypo name="calendar" color="#2F55D0" size={40} />
+      </WapperInput>
+
+      <Input
+        style={styles.boxShadow}
+        placeholder="Descrição"
+        placeholderTextColor="#999CA6"
+      />
+      <Button
+        style={styles.boxShadow}
+        title="Cadastro"
+        type="button"
+        onPress={() => navigation.navigate('Tab', { screen: 'Main' })}
+      >
+        <TextButton>Entrar</TextButton>
+      </Button>
+    </Container>
   );
 };
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
+    elevation: 5,
+  },
+});
 export default EventRegister;
---
+++ src/pages/EventRegister/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+  border: none;
+  width: 280px;
+  height: 42px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 8px;
+  background-color: #fff;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 30px;
+  background-color: #2f55d0;
+  color: #fff;
+  align-items: center;
+  justify-content: center;
+  width: 280px;
+  height: 62px;
+  border-radius: 8px;
+`;
+export const TextButton = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const Container = styled.KeyboardAvoidingView`
+  background-color: #fff;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+export const ViewLogo = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+  flex-direction: row;
+`;
+export const Textlogo = styled.Text`
+  color: #2f55d0;
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 50px;
+`;
+
+export const WapperInput = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+export const InputTickets = styled.TextInput`
+  border: none;
+  width: 120px;
+  height: 42px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 8px;
+  background-color: #fff;
+  margin-right: 20px;
+`;
+export const InputPrice = styled.TextInput`
+  border: none;
+  width: 120px;
+  height: 42px;
+  padding: 10px;
+  margin-top: 10px;
+  margin-left: 20px;
+  border-radius: 8px;
+  background-color: #fff;
+`;
+export const InputNumber = styled.TextInput`
+  border: none;
+  width: 120px;
+  height: 42px;
+  padding: 10px;
+  margin-top: 10px;
+  margin-left: 20px;
+  border-radius: 8px;
+  background-color: #fff;
+`;
