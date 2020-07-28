fd1768cd6fe00b0e62ac4ebec88b84f09d97d7f5
hjunor <hjunor@gmail.com>
2020-07-27 21:04:14 -0300

    add: register screen

--- src/pages/Event/index.js
+++ src/pages/Event/index.js
@@ -1,14 +1,80 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-// import { Container } from './styles';
-
-const Event = () => {
+import {
+  Container,
+  WrapperEvent,
+  Image,
+  WrapperText,
+  TextData,
+  TextGrey,
+  TextRed,
+  Text,
+  TextGreen,
+  WrapperTextData,
+  WrapperImage,
+  WrapperData,
+  Button,
+  TextButton,
+} from './styles';
+import image from '../../assets/app.jpeg';
+const space = '                       ';
+const Event = ({ navigation }) => {
   return (
-    <View>
-      <Text>EventRegister</Text>
-    </View>
+    <Container>
+      <WrapperEvent style={styles.boxShadow}>
+        <WrapperImage>
+          <Image source={image} />
+          <WrapperText style={styles.boxShadow}>
+            <Text>Workshoop á arte de saber Desvendando Paradigmas!</Text>
+          </WrapperText>
+        </WrapperImage>
+        <View>
+          <WrapperTextData>
+            <TextData>Data:</TextData>
+            <TextRed> 18 out - 2020</TextRed>
+            <TextData style={{ left: 33 }}>Tipo:</TextData>
+            <TextGreen style={{ left: 38 }}>Online</TextGreen>
+          </WrapperTextData>
+          <WrapperData>
+            <TextData>Description:</TextData>
+            <TextGrey style={{ right: 75 }}>
+              {`${space}`}workshoop desenvolvido para agregar suas diferenças
+              pessoais e allkndllimentar novas sircustancias espirituais na
+              tecnologia.
+            </TextGrey>
+          </WrapperData>
+          <WrapperTextData>
+            <TextData>Igressos:</TextData>
+            <TextRed>70 / 100</TextRed>
+
+            <TextData style={{ left: 33 }}>Categoria:</TextData>
+            <TextGreen style={{ left: 38 }}>Social</TextGreen>
+          </WrapperTextData>
+        </View>
+        <Button
+          style={styles.boxShadow}
+          title="Cadastro"
+          type="button"
+          onPress={() => navigation.navigate('Tab', { screen: 'Main' })}
+        >
+          <TextButton>Conprar</TextButton>
+        </Button>
+      </WrapperEvent>
+    </Container>
   );
 };
+const styles = StyleSheet.create({
+  boxShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
+    elevation: 5,
+  },
+});
 export default Event;
--- src/pages/Event/styles.js
+++ src/pages/Event/styles.js
@@ -1,3 +1,84 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  align-items: center;
+  background-color: #fff;
+  padding: 40px;
+  flex: 1;
+`;
+export const WrapperEvent = styled.View`
+  background-color: #fff;
+  width: 360px;
+  height: 480px;
+  border-radius: 8px;
+  margin-top: 0px;
+  align-items: center;
+`;
+export const Image = styled.Image`
+  width: 330px;
+  height: 168px;
+  border-radius: 8px;
+`;
+export const Text = styled.Text`
+  color: #fff;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+`;
+export const WrapperText = styled.View`
+  bottom: 30px;
+  background-color: #1a1946;
+  color: #fff;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  width: 310px;
+  height: 100px;
+`;
+export const WrapperImage = styled.View`
+  margin-top: 24px;
+  align-items: center;
+  background-color: #fff;
+`;
+export const TextData = styled.Text`
+  color: #1a1946;
+  font-weight: bold;
+`;
+export const TextRed = styled.Text`
+  color: #d04c2f;
+  font-weight: bold;
+  left: 4px;
+`;
+export const TextGrey = styled.Text`
+  color: #9296a3;
+  text-align: left;
+  font-weight: bold;
+`;
+export const TextGreen = styled.Text`
+  color: #308522;
+  font-weight: bold;
+`;
+export const WrapperTextData = styled.View`
+  flex-direction: row;
+  width: 330px;
+`;
+export const WrapperData = styled.View`
+  flex-direction: row;
+  width: 330px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 15px;
+  background-color: #1a1946;
+  color: #fff;
+  align-items: center;
+  justify-content: center;
+  width: 160px;
+  height: 62px;
+  border-radius: 8px;
+`;
+export const TextButton = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+`;
